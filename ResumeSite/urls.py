22b55d7ac51f6325e878fdebded142a22a24737b
suprit08 <sonarsuprit@gmail.com>
2021-01-20 15:36:38 +0530

    Initial Release

---
+++ ResumeSite/urls.py
@@ -0,0 +1,27 @@
+"""ResumeSite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="home"),
+]
+
+# handler404 = 'views.error_404'
+# handler500 = 'views.error_500'
+# handler403 = 'views.error_403'
+# handler400 = 'views.error_400'
